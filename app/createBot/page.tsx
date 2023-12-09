3f43331466601e7a82543551cc9b30ad1d83ae6b
unknown <Yuvraj>
2023-12-09 14:49:22 +0530

    Created creation of bot page

--- app/createBot/page.tsx
+++ app/createBot/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import {
   Avatar,
   Box,
@@ -5,6 +6,9 @@ import {
   Center,
   FileInput,
   Flex,
+  RangeSlider,
+  Slider,
+  Stack,
   Text,
   TextInput,
   Textarea,
@@ -12,6 +16,7 @@ import {
 } from '@mantine/core';
 
 import {
+  IconCircleCheck,
   IconCirclesRelation,
   IconHandRock,
   IconMan,
@@ -73,7 +78,7 @@ function page({}: Props) {
             select avatar from the system.
           </Text>
           <Flex align={'center'} gap={15} mt={15} className={S.inputFile}>
-            <Button leftSection={<IconRobot size={20} />}>
+            <Button leftSection={<IconCircleCheck size={20} />}>
               Let the app select picture for you.
             </Button>
             <Text>OR</Text>
@@ -88,14 +93,35 @@ function page({}: Props) {
           <Center w={40} h={40} bg={'#2C3C49'} style={{ borderRadius: '7px' }}>
             <IconCirclesRelation size={20} color={'#74C0FC'} />
           </Center>
-          <Text fw={'bold'}>Greeting</Text>
+          <Text fw={'bold'}>Emotions</Text>
         </Flex>
         <Text c={'dimmed'} fz={13} mb={5} mt={3}>
-          What would they say to introduce themselves? For example, "Jethalal Gada" could say:
-          "Mujhe babita ji pasand hai aur mujhe jelebi phapdha khana pasand hai".
+          The character will dynamically respond and engage with the user, tailoring its reactions
+          and dialogue in accordance with the specified levels of emotions.{' '}
         </Text>
-        <Textarea placeholder="eg: Sup man! I like potatos" rows={6} />
+        <Stack w={200} mt={20}>
+          <Box>
+            <Text fw={'bold'}>Anger</Text>
+            <Slider defaultValue={20} label={(value) => `${value} %`} mt={5} />
+          </Box>
+          <Box>
+            <Text fw={'bold'}>Rudeness</Text>
+            <Slider defaultValue={15} label={(value) => `${value} %`} mt={5} />
+          </Box>
+          <Box>
+            <Text fw={'bold'}>Kindness</Text>
+            <Slider defaultValue={70} label={(value) => `${value} %`} mt={5} />
+          </Box>
+          <Box>
+            <Text fw={'bold'}>Excitement</Text>
+            <Slider defaultValue={40} label={(value) => `${value} %`} mt={5} />
+          </Box>
+        </Stack>
       </Box>
+      {/* Create bot  */}
+      <Flex w={'100%'} justify={'end'} mt={20}>
+        <Button leftSection={<IconRobot size={20} />}>Create Character!</Button>
+      </Flex>
     </Box>
   );
 }
